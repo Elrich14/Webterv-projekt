2d4dae0c999957a4a26dec69af81665bf4412929
Norbert <96nucu@gmail.com>
2024-04-18 00:23:20 +0200

    navigation css fixed and navigation modifications

--- beadando/php/nav.php
+++ beadando/php/nav.php
@@ -14,8 +14,8 @@ if ($directory === './') {
 ?>
 
 <header id="navbar">
+    <a href="<?php echo $folder;?>/index.php"><img <?php if ($page === 'index.php') echo 'class="logo active"'; else echo 'class="logo"' ?> src="<?php echo $folder;?>/media/image/logo_without_bg.webp" alt="logo"></a>
     <div>
-        <a href="<?php echo $folder;?>/index.php"><img <?php if ($page === 'index.php') echo 'class="logo active"'; else echo 'class="logo"' ?> src="<?php echo $folder;?>/media/image/logo_without_bg.webp" alt="logo"></a>
         <nav>
             <ul class="nav_links">
                 <li> <a <?php if ($page === 'designer.php') echo 'class="active"'; ?> href="<?php echo $folder;?>/designer.php">Dizájnerek</a></li>
@@ -27,17 +27,11 @@ if ($directory === './') {
     </div>
 
     <div class="profil">
-        <a <?php if ($page === 'login.php') echo 'class="active"'; ?> href="<?php echo $folder;?>/login.php"><img class="loginimg" src="<?php echo $folder;?>/media/image/user.webp" alt="belépés"></a>
-        <a <?php if ($page === 'kosar.php') echo 'class="active"'; ?> href="<?php echo $folder;?>/kosar.php"><img class="kosar" src="<?php echo $folder;?>/media/image/shopping-cart.webp" alt="kosár"></a>
+        <a <?php if ($page === 'kosar.php') echo 'class="activeround"'; ?> href="<?php echo $folder;?>/kosar.php"><img class="kosar" src="<?php echo $folder;?>/media/image/shopping-cart.webp" alt="kosár"></a>
+        <a <?php if ($page === 'login.php') echo 'class="activeround"'; ?> href="<?php echo $folder;?>/login.php"><img class="loginimg" src="<?php echo $folder;?>/media/image/user.webp" alt="belépés"></a>
+        <?php if (isset($_SESSION["user"])) { ?>
+            <a href="<?php echo $folder;?>/logout.php"><img class="logout" src="<?php echo $folder;?>/media/image/exit.webp" alt="kilépés"></a>
+        <?php }?>
     </div>
-
-    
-    <?php if (isset($_SESSION["user"])) { ?>
-    <a href="<?php echo $folder;?>/profil.php">Profilom</a>
-    <a href="<?php echo $folder;?>/logout.php">Kijelentkezés</a>
-    <?php } else { ?>
-    <a href="<?php echo $folder;?>/login.php">Bejelentkezés</a>
-    <a href="<?php echo $folder;?>/signup.php">Regisztráció</a>
-    <?php } ?>
 
 </header>
